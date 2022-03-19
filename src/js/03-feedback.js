c917d023da7aa84acc2e806c596e343b6e7b8c69
cs050383bav <87935875+cs050383bav@users.noreply.github.com>
2022-03-19 22:46:15 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function updateForm() {
+  const saveData = localStorage.getItem(STORAGE_KEY);
+  const { email, message } = JSON.parse(saveData);
+  form.email.value = email;
+  form.message.value = message;
+  formData.email = email;
+  formData.message = message;
+}
+
+function onFormInput(evt) {
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const formDataToSend = new FormData(event.currentTarget);
+  formDataToSend.forEach((value, name) => {
+    formData[name] = value;
+  });
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
