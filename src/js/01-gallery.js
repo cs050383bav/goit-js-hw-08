ea252d3e17893c741ba4ed1fd8caeb1a48fcdef5
cs050383bav <87935875+cs050383bav@users.noreply.github.com>
2022-03-20 14:03:52 +0200

    update 1 and2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,8 @@
 
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 
 const makeGalleryItemMarkup = ({ preview, original, description }) => {
   return `
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,21 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const KEY = 'videoplayer-current-time';
+const STORAGE_KEY = 'videoplayer-current-time';
 
 const player = new Player('vimeo-player');
-const key = localStorage.getItem(KEY);
+const key = localStorage.getItem(STORAGE_KEY);
+
 if (key) {
     player.setCurrentTime(parseFloat(key));
   }
 
+  
+
+  player.on(
+    'timeupdate',
+    throttle(data => {
+      localStorage.setItem(STORAGE_KEY, data.seconds.toString());
+    }, 1000),
+  );
   
